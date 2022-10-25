a1cfd91ad65a7bcc17961879ffc4974ab45a7a5f
K. Burak Ozdemir <kburakozdemir@gmail.com>
2022-10-25 12:03:08 +0300

    Add composer

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 
-echo "test";
+/**
+ * @file
+ */
 
-phpinfo();
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$headers = [
+  'Content-Type' => 'application/json',
+  'X-Hasura-Admin-Secret' => $_ENV['XHASURAADMINSECRET'],
+];
+
+$client = new Client([
+  'headers' => $headers,
+]);
+
+
+$response = $client->get($_ENV['XHASURAURLLRT']);
+
+print_r($response->getBody()->getContents());
+
+$response = $client->get($_ENV['XHASURAURLSEC']);
+
+print_r($response->getBody()->getContents());
