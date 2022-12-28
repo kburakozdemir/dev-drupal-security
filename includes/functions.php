c9a2d17555a8161858d652b2cbc163ccbeae6997
K. Burak Ozdemir <kburakozdemir@gmail.com>
2022-12-28 21:17:40 +0300

    Format code

--- includes/functions.php
+++ includes/functions.php
@@ -8,13 +8,14 @@
 /**
  * Converts object to array.
  */
-function object_to_array($data) {
-  if (is_array($data) || is_object($data)) {
-    $result = [];
-    foreach ($data as $key => $value) {
-      $result[$key] = (is_array($value) || is_object($value)) ? object_to_array($value) : $value;
+function object_to_array($data)
+{
+    if (is_array($data) || is_object($data)) {
+        $result = [];
+        foreach ($data as $key => $value) {
+            $result[$key] = (is_array($value) || is_object($value)) ? object_to_array($value) : $value;
+        }
+        return $result;
     }
-    return $result;
-  }
-  return $data;
+    return $data;
 }
